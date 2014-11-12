830a7e7d55df29b5696e8a1fc7341eee0b393a3b
amanurat <assanai17@gmail.com>
2014-11-12 23:48:58 +0700

    Add upcasting, downcasting

--- src/main/java/com/training/oop/casting/upcasting/B.java
+++ src/main/java/com/training/oop/casting/upcasting/B.java
@@ -7,6 +7,7 @@ class A {
     public void print() {
         System.out.println("Hello from class A");
     }
+    public void show() { System.out.println("Show from class A");}
 }
 public class B extends A {
     public void print() {
@@ -14,10 +15,25 @@ public class B extends A {
     }
 
     public static void main(String[] args) {
+        //Once you have upcast an object reference, you can access only the fields and methods declared by the base class.
         A obj = new B();
         obj.print();
+        obj.show();
 
-        B bObj = (B) new A();
+        B bObj = new B();
         bObj.print();
+        bObj.show();
+
+        A aObj = new A();
+        aObj.print();
+        aObj.show();
+
+        //An upcast reference can be downcast to a subclass through explicit casting.
+        A aa = new B();
+
+        B bb = (B) aa;
+        bb.print();
+
+
     }
 }
